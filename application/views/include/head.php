9f91f8d0645a08bce228ad435b7255bb94174a31
mawei <mawei78951@126.com>
2014-12-31 00:06:32 +0800

    head
    head

--- application/views/include/head.php
+++ application/views/include/head.php
@@ -1,5 +1,6 @@
 <head>
 	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<script src="<?php echo base_url('assets/static/js/jquerymobile/jquery-1.10.2.min.js')?>"></script>
 	<link rel="stylesheet" href="<?php echo base_url('assets/static/js/jquerymobile/jquery.mobile-1.4.1.min.css')?>">
 	<script src="<?php echo base_url('assets/static/js/jquerymobile/jquery.mobile-1.4.1.min.js')?>"></script>
