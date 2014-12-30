e0018341f3305a503026f34c4f68f5e719fc2ba5
mawei <mawei78951@126.com>
2014-12-30 23:51:43 +0800

    sd
    sd

--- application/views/login.php
+++ application/views/login.php
@@ -6,7 +6,7 @@
 <div data-role="page">
   <div data-role="content">
    {error}
-    <form method="post" action="<?php echo base_url("index/loginpost");?>" data-ajax="false">
+    <form method="post" action="<?php echo site_url("index/loginpost");?>" data-ajax="false">
       <input type="text" name="username" id="username" placeholder="用户名">
       <input type="text" name="password" id="password" placeholder="密码">
       <input type="submit" data-inline="false" value="提交">
