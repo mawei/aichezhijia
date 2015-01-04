921f0df0588e8ea6de03263a428c9ef7208ca2d6
mawei <mawei78951@126.com>
2015-01-04 13:44:29 +0800

    32
    23

--- application/views/login.php
+++ application/views/login.php
@@ -8,7 +8,7 @@
    {error}
     <form method="post" action="<?php echo site_url("index/loginpost");?>" data-ajax="false">
       <input type="text" name="username" id="username" placeholder="用户名">
-      <input type="text" name="password" id="password" placeholder="密码">
+      <input type="password" name="password" id="password" placeholder="密码">
       <input type="hidden" name="weixinID" id="weixinID" >
       <input type="submit" data-inline="false" value="提交">
     </form>
--- application/views/register.php
+++ application/views/register.php
@@ -10,8 +10,8 @@
       <input type="text" name="username" id="username" placeholder="用户名">
       <input type="text" name="phone" id="phone" placeholder="手机号">
       <input type="text" name="carmodel" id="carmodel" placeholder="车型">
-      <input type="text" name="password" id="password" placeholder="密码">
-      <input type="text" name="password2" id="password2" placeholder="再次密码">
+      <input type="password" name="password" id="password" placeholder="密码">
+      <input type="password" name="password2" id="password2" placeholder="再次密码">
       <input type="submit" data-inline="false" value="注册">
     </form>
   </div>
