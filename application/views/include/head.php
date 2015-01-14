c97fb713740684965c4d6570a924de823efebf1f
mawei <mawei78951@126.com>
2015-01-14 12:56:55 +0800

    2
    2

--- application/views/include/head.php
+++ application/views/include/head.php
@@ -3,7 +3,7 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 		<link rel="stylesheet" type="text/css" href="<?=base_url('assets/css/bootstrap.min.css')?>"/>
-		<link rel="stylesheet" type="text/css" href="<?=base_url('assets/css/custom.css')?>"/>		
+		<link rel="stylesheet" type="text/css" href="<?=base_url('assets/css/custom.css')?>?id=<?=mt_rand(10000000,99999999)?>"/>		
 		<script src="<?=base_url('assets/js/jquery-1.11.0.min.js')?>"></script> 
 		<script src="<?=base_url('assets/js/bootstrap.min.js')?>"></script>
 		<script src="<?=base_url('assets/js/myscript.js')?>"></script>
