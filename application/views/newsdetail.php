03de5d383ffd99b12c12a0595099cd15fa84254c
mawei <mawei78951@126.com>
2015-01-14 12:38:50 +0800

    2
    2

--- application/views/newsdetail.php
+++ application/views/newsdetail.php
@@ -6,7 +6,7 @@
 
 {header}
 <div data-role="page">
-  
+  <a href="javascript:history.go(-1);" class="btn btn-primary" ><font style="color: white">返回</font></a>
   <div class="jumbotron">
   <center><h3><?=$news['title']?></h3></center>
   <br/>
