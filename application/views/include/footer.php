2340e0b1ae737f1270dc1354f9743e40f1ee5820
mawei <mawei78951@126.com>
2015-02-13 00:31:24 +0800

    12
    12

--- application/views/include/footer.php
+++ application/views/include/footer.php
@@ -1,17 +1,6 @@
-<!-- <div class="container footer"> -->
-<!-- 				<div class="col-xs-12 col-md-8 noPadding"> -->
-<!-- 				</div> -->
-<!-- 				<div class="col-xs-12 col-md-4 noPadding"> -->
-<!-- 					<div class="socialButtons responsiveAlign"> -->
-<!-- 						 <a href="#" class="noPadding"> -->
-<!-- 							<img src="assets/images/twitter.svg" alt="image1"> -->
-<!-- 						</a> -->
-<!-- 						<a href="#" class="noPadding"> -->
-<!-- 							<img src="assets/images/facebook.svg" alt="image1"> -->
-<!-- 						</a> -->
-<!-- 						<a href="#" class="noPadding"> -->
-<!-- 							<img src="assets/images/behance.svg" alt="image1"> -->
-<!-- 						</a> -->
-<!--  					</div> -->
-<!-- 				</div> -->
-<!-- 			</div> -->
+<div class="container footer">
+				<div class="col-xs-12 col-md-12 noPadding" style="text-align:center">
+				©2015泉林汽车    沪ICP备15007130号
+				</div>
+				
+			</div>
--- application/views/include/head.php
+++ application/views/include/head.php
@@ -1,6 +1,7 @@
 <head>
 		<title>泉林汽车</title>
 		<meta charset="utf-8">
+		<meta name="baidu-site-verification" content="Qjt8hkg8P2" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 		<link rel="stylesheet" type="text/css" href="<?=base_url('assets/css/bootstrap.min.css')?>"/>
 		<link rel="stylesheet" type="text/css" href="<?=base_url('assets/css/custom.css')?>?id=<?=mt_rand(10000000,99999999)?>"/>		
