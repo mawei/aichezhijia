6ed7478e20a0d9c619743fa726c44dbd024889ff
mawei <mawei78951@126.com>
2015-03-24 20:25:29 +0800

    a

--- application/controllers/index.php
+++ application/controllers/index.php
@@ -275,7 +275,7 @@ class index extends CI_Controller {
 			$this->data['signType'] = 'MD5';
 			$this->data['package'] = "prepay_id=".$result->prepay_id;
 						
-			$string = "appid={$this->data['appId']}&nonceStr={$this->data['nonceStr']}&package={$this->data['package']}&signType=MD5&timeStamp={$this->data['timeStamp']}";
+			$string = "appId={$this->data['appId']}&nonceStr={$this->data['nonceStr']}&package={$this->data['package']}&signType=MD5&timeStamp={$this->data['timeStamp']}";
 			$string = $string . "&key=7c914cc19e472a13e7b93aad9aa7bc69";
 			$this->data['paySign'] = strtoupper(MD5($string));
 				
