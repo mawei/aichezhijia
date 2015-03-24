a27f92ae2ae8646dfe466616cac7544bafd1721e
mawei <mawei78951@126.com>
2015-03-24 19:10:26 +0800

    a

--- application/views/order_confirm.php
+++ application/views/order_confirm.php
@@ -3,14 +3,16 @@
 {head}
 <script type="text/javascript">
 function onBridgeReady(){
+		alert('1');
+	
 	   WeixinJSBridge.invoke(
 	       'getBrandWCPayRequest', {
-	           "appId" : {appid},     //公众号名称，由商户传入     
-	           "timeStamp":{timeStamp},         //时间戳，自1970年以来的秒数     
-	           "nonceStr" : {nonce_str}, //随机串     
-	           "package" : {package},     
+	           "appId" : "{appid}",     //公众号名称，由商户传入     
+	           "timeStamp":"{timeStamp}",         //时间戳，自1970年以来的秒数     
+	           "nonceStr" : "{nonce_str}", //随机串     
+	           "package" : "{package}",     
 	           "signType" : "MD5",         //微信签名方式:     
-	           "paySign" : {paySign} //微信签名 
+	           "paySign" : "{paySign}" //微信签名 
 	       },
 	       function(res){     
 	           if(res.err_msg == "get_brand_wcpay_request:ok" ) {}     // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。 
@@ -27,7 +29,7 @@ function onBridgeReady(){
 	}else{
 	   onBridgeReady();
 	}
-$("#getBrandWCPayRequest").onclick(onBridgeReady());
+$("#getBrandWCPayRequest").click(onBridgeReady());
 </script>
 <body>
 {header}
